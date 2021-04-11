f1459d3baf8a3410070952aaf71163481a2b977d
Amy Hacker <lilblue2005@hotmail.co.uk>
2021-04-11 08:39:37 +0100

    Change Board properties to reflect child state for history.

--- src/index.js
+++ src/index.js
@@ -29,8 +29,8 @@ class Board extends React.Component {
   renderSquare(i) {
     return  (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
@@ -72,7 +72,7 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history[{
+      history: [{
         squares: Array(9).fill(null),
       }],
       xIsNext: true,
