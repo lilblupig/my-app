37ffe650bd302afaa9ca580ae839f00fec43bfa8
Amy Hacker <lilblue2005@hotmail.co.uk>
2021-04-11 09:07:39 +0100

    Remove rogue comma from Game handleClick.

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{
-        squares: squares,
+        squares: squares
       }]),
       xIsNext: !this.state.xIsNext,
     });
