9f47dd55c521b9eb9469e2aee0b2b70a79faf195
Amy Hacker <lilblue2005@hotmail.co.uk>
2021-04-11 09:19:44 +0100

    Remove extra space from Board renderSquare.

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function Square(props) {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return  (
+    return (
       <Square
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
