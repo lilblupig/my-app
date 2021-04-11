d2713b2e9c0032f8fa6a6578d16970d4aa71d8c2
Amy Hacker <lilblue2005@hotmail.co.uk>
2021-04-11 08:11:52 +0100

    Add different turns.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function Square(props) {
   return (
     <button
     className="square"
-    onClick={props.onClick()}
+    onClick={props.onClick}
     >
       {props.value}
     </button>
@@ -17,14 +17,18 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null,)
+      squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'x';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'x' : 'o';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
